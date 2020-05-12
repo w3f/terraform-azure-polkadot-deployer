d081f541a782c9fb28c9aef9ca86fe1e9b4ff64b
Vladimir <vladimir.d.babichev@gmail.com>
2020-05-12 13:29:39 +0100

    Removed backend and state configuration from module

--- variables.tf
+++ variables.tf
@@ -14,12 +14,6 @@ variable "cluster_name" {
   type        = string
 }
 
-variable "deployment_name" {
-  description = "Name of this polkadot deployment"
-  default     = "polkadot"
-  type        = string
-}
-
 variable "location" {
   description = "Azure location"
   type        = string
@@ -40,15 +34,5 @@ variable "machine_type" {
 variable "k8s_version" {
   description = "Kubernetes version to use for the AKS cluster"
   default     = "1.15.10"
-  type        = string
-}
-
-variable "gcp_project_id" {
-  description = "Google cloud project id used for terraform state"
-  type        = string
-}
-
-variable "gcp_credentials" {
-  description = "Either the path to or the contents of a GCP service account key file in JSON format"
   type        = string
 }
