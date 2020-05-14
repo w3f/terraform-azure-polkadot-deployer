7eebf05a0368081133b301061f032b1abef089aa
Vladimir <vladimir.d.babichev@gmail.com>
2020-05-14 15:03:14 +0100

    Removed unused resource

--- main.tf
+++ main.tf
@@ -51,14 +51,6 @@ resource "azurerm_subnet" "polkadot-aks" {
   address_prefix       = "10.0.1.0/24"
 }
 
-resource "azurerm_public_ip" "polkadot-aks" {
-  name                = "polkadot-${var.cluster_name}"
-  location            = azurerm_resource_group.polkadot-aks.location
-  resource_group_name = azurerm_resource_group.polkadot-aks.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
-
 resource "azurerm_network_security_group" "polkadot-aks" {
   name                = "polkadot-${var.cluster_name}"
   location            = azurerm_resource_group.polkadot-aks.location
